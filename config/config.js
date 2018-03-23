1229ed380a12a399663bd97a4cedd04eee46b25c
enjoyass@outlook.com <enjoyass@outlook.com>
2018-03-23 20:42:19 +0800

    分离封装文件

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var config = {
     wechat: {
         appID: "wx63e04fe98fc74772",
         appSecret: "8241a22c4bbca8fcc2863bb2b000930d",
-        token: "wanmeichuangyou"
+        token: "guangxinxiliujianjun"
     },
     server: {
         port: 1234
